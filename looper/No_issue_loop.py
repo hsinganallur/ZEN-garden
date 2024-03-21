8bbe5375c6d52b32910bbb2b5386dd5d901b90a3
hsinganallur <118671326+hsinganallur@users.noreply.github.com>
2024-03-21 12:00:37 +0100

    Push 11

--- looper/No_issue_loop.py
+++ looper/No_issue_loop.py
@@ -1,40 +1,18 @@
-import subprocess
-from zen_garden.postprocess.results import Results
-def run_zen_garden(config_file="./config.py", dataset=None, job_index=None):
-    # Construct the command to run the ZEN-Garden module
-    command = ["python", "-m", "zen_garden", "--config", config_file]
-
-    if dataset:
-        command.extend(["--dataset", dataset])
-
-    if job_index:
-        command.extend(["--job_index", job_index])
-
-    # Run the ZEN-Garden module
-    subprocess.run(command)
-
-
-def analyze_results(results_folder):
-    # Analyze the results using the Results class
-    print("Before Results")
-    results = Results(results_folder)
-    print("After Results")
-    return results
-
-    #Example: Get the dataframe for a specific component
-    #component_name = "capacity"
-    #dataframe = results.get_df(component_name)
-
-
-if __name__ == "__main__":
-    # Specify the configuration file, dataset, and job index if needed
-    config_file = "C:\GitHub\ZEN-garden\data\config.py"
-    dataset_path = "C:\GitHub\ZEN-garden\data\looping_test_folder"
-    #job_index = "your_job_index"
-
-    # Run ZEN-Garden module
-    run_zen_garden(config_file, dataset_path)
-
-    # Analyze results
-    results_folder = "C:\GitHub\ZEN-garden\data\outputs\looping_test_folder"
-    analyze_results(results_folder)
+import pandas as pd
+# Retrieve data and create DataFrames
+df_a = r.get_full_ts("flow_storage_charge")
+df_b = r.get_df("flow_storage_charge")
+df_c = r.get_total("flow_storage_charge")
+# Convert to DataFrames
+df_a = pd.DataFrame(df_a)
+df_b = pd.DataFrame(df_b)
+df_c = pd.DataFrame(df_c)
+# Define file paths
+file_path_a = "C:\\GitHub\\ZEN-garden\\looper\\other_tests\\flow_storage_charge_full_ts.csv"
+file_path_b = "C:\\GitHub\\ZEN-garden\\looper\\other_tests\\flow_storage_charge_get_df.csv"
+file_path_c = "C:\\GitHub\\ZEN-garden\\looper\\other_tests\\flow_storage_charge_get_total.csv"
+# Save DataFrames to CSV files
+df_a.to_csv(file_path_a, index=True, mode='w')
+df_b.to_csv(file_path_b, index=True, mode='w')
+df_c.to_csv(file_path_c, index=True, mode='w')
+print("CSV files saved successfully.")
